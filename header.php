c6d97e375bb83a5383fd6fe74555b77d42ebd502
irutaxcel <52888285+irutaxcel@users.noreply.github.com>
2020-06-24 02:39:36 +0200

    Update header.php

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
             </div>
             <!-- Primary Navigation -->
             <nav class="navbar navbar-expand-lg navbar-dark color-second-bg">
-                <a class="navbar-brand" href="#">Mobile Shopee</a>
+                <a class="navbar-brand" href="#">Mobile Shope</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
